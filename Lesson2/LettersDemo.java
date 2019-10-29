45363ab4a50f21dbf1e9b961ee101c493f8fbf4f
ktamerl4 <47120319+ELA-elaborate-language-align@users.noreply.github.com>
2019-10-29 13:55:31 -0400

    Add files via upload

---
+++ Lesson2/LettersDemo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class LettersDemo {
+	public static void main(String[] args)
+	{
+
+		Scanner input = new Scanner(System.in);
+		String textLine;
+		System.out.print("Enter a line of text:");
+		textLine = input.nextLine();
+		System.out.println("Text in uppercase:"+textLine.toUpperCase());
+		System.out.println("Text in lowercase:"+textLine.toLowerCase());
+	}
+}
